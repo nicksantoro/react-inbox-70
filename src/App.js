a9f4728ad9c238787eada50f504a7467ff013d7a
Nick Santoro <nicholassantoro@Nicholass-MacBook-Pro.local>
2018-11-14 19:37:53 -0800

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'font-awesome/css/font-awesome.css';
 import './App.css';
+
+import MessageList from './components/MessageList'
+import Toolbar from './components/Toolbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-         
+        <Toolbar />
+        <MessageList />
       </div>
     );
   }
