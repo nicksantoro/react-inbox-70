8d519d0e105a92ae01f2256ffeeb347194e8ff9d
Nick Santoro <nicholassantoro@Nicholass-MacBook-Pro.local>
2018-11-14 20:31:16 -0800

    4th commit

--- src/components/Message.js
+++ src/components/Message.js
@@ -12,13 +12,15 @@ export default class Message extends Component {
                 <input type="checkbox" />
               </div>
               <div className="col-xs-2">
-                <i className="star fa fa-star-o"></i>
+        
+                <i className={`star fa fa-star${this.props.message.starred ? "" : "-o"}`}></i>
               </div>
             </div>
           </div>
           <div className="col-xs-11">
             <a href="#">
-              Here is some message text that has a bunch of stuff
+              {/* Here is some message text that has a bunch of stuff */}
+              {this.props.message.subject}
             </a>
           </div>
         </div>
